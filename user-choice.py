6a0e2ccab2fce4937d964f93f32afc98342f1caa
Mireri <robertbjanam@gmail.com>
2026-09-12 20:18:23 +0300

    Refactor Monty Hall problem implementation
    Before this implementation, I faced two challenges. The first problem was that the reveal wasn't constrained by the prize. Generally, the host must open a door that is both unchosen by the user and is empty. The prize door is selected after an empty door is removed. This means that the act of opening a random_prize_door isn't driven by avoiding the prize.
    This also leads to the second problem. The prize assignment logic shifts the probabilities. Because the prize is chosen at the end after a door is removed, the probability is forced into a 50/50 situation instead of 2/3 when switching and 1/3 when not.
    How did i fix this? By assigning the prize at the beginning instead of the end. But even this implementation, though logically correct, still needs some work (repetitions are everywhere!)

--- user-choice.py
+++ user-choice.py
@@ -9,67 +9,61 @@
 import random
 
 def main():
-    prior_doors = ["1", "2", "3"]
-    final_doors = []
+    doors = ["1", "2", "3"]
     
-    try:
-        pick_door = int(input("Pick a door (1, 2, 3): "))
-        
-        if not (1 <= pick_door <= 3):
-            print("Pick a number between 1 and 3.")
-            return
-            
-        for door in prior_doors:
-            if str(pick_door) == door:
-                final_doors.append(door)
-                prior_doors.remove(door)
-                #print(prior_doors)
-                break
-        
-        random_empty_door = random.choice(prior_doors)
-        print(f"Door {random_empty_door} is opened and is empty.")
-        for door in prior_doors:
-            if random_empty_door == door:
-                prior_doors.remove(door)
+    prize_door = random.choice(doors)
+    
+    while True:
+        try:
+            pick_door = int(input("Pick a door (1, 2, 3): "))
+            if not (1 <= pick_door <= 3):
+                print("Pick a number between 1 and 3.")
                 continue
-            final_doors.append(door)
-        
-        random_prize_door = random.choice(final_doors)
-        while True:
-            choice_to_switch = input("Do you wish to switch doors? (y/n): ").lower().strip()
             
-            if choice_to_switch == 'n':
-                if random_prize_door == str(pick_door):
-                    print(f"You won! The prize was in door {pick_door}.")
-                    print("You didn't switch and you WON.")
-                else:
-                    for door in final_doors:
-                        if pick_door == door:
-                            final_doors.remove(door)
-                            print(final_doors)
-                            break
+            if str(pick_door) == prize_door:
+                temp_doors = []
+                for door in doors:
+                    if door != str(pick_door):
+                        temp_doors.append(door)
+                
+                random_empty_door = random.choice(temp_doors)
+                
+                print(f"Door {random_empty_door} is opened and is empty.")
+                while True:
+                    choice_to_switch = input("Do you wish to switch doors? (y/n): ").lower()
                     
-                    print(f"Sorry, the prize was in door {door}.")
-                    print("You didn't switch and you LOST.")
-                break
-            elif choice_to_switch == 'y':
-                if random_prize_door == str(pick_door):
-                    print(f"Sorry, the prize was in door {pick_door}.")
-                    print("You switched and you LOST.")
-                else:
-                    for door in final_doors:
-                        if random_prize_door == door:
-                            final_doors.remove(random_prize_door) 
-                            break
-                    
-                    print(f"You won! The prize was in door {door}.")
-                    print("You switched and you WON.")
-                break
+                    if choice_to_switch == 'n':
+                        print(f"You won! The prize was in door {prize_door}.")
+                        print("You didn't switch and you WON.")
+                        break
+                    elif choice_to_switch == 'y':
+                        print(f"Sorry, the prize was in door {prize_door}.")
+                        print("You switched and you LOST.")
+                        break
+                    else:
+                        print("Pick between y or n.")
+            
             else:
-                print("Pick between y or n.")
-        
-    except ValueError:
-        print("Pick a number.")
+                for door in doors:
+                    if door != prize_door and door != str(pick_door):
+                        print(f"Door {door} is opened and is empty.")
+                        break
+                while True:
+                    choice_to_switch = input("Do you wish to switch doors? (y/n): ").lower()
+                    
+                    if choice_to_switch == 'n':
+                        print(f"Sorry, the prize was in door {prize_door}.")
+                        print("You didn't switch and you LOST.")
+                        break
+                    elif choice_to_switch == 'y':
+                        print(f"You won! The prize was in door {prize_door}.")
+                        print("You switched and you WON.")
+                        break
+                    else:
+                        print("Pick between y or n.")
+            break
+        except ValueError:
+            print("Pick a number.")
     
 if __name__ == "__main__":
     main()
